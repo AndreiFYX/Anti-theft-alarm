c6197576e064fb162ad65696aa2023b376cb6a8a
AndreiFYX <andrej.fyx@mail.ru>
2026-08-09 13:48:53 +0500

    update scripts and mover

---
+++ Assets/Scripts/ChestOpenTrigger.cs
@@ -0,0 +1,34 @@
+using Assets.Scripts;
+using UnityEngine;
+
+public class ChestOpenTrigger : MonoBehaviour
+{
+    [SerializeField] private OpenDoor _chest;
+
+    private bool _isOpened = false;
+    private bool _hasOpener;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<ChestOpener>())
+            _hasOpener = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<ChestOpener>())
+            _hasOpener = false;
+    }
+
+    private void Update()
+    {
+        if (_isOpened)
+            return;
+
+        if( _hasOpener && Input.GetKeyDown(KeyCode.F))
+        {
+            _chest.Open();
+            _isOpened = true;
+        }
+    }
+}
---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class OpenDoor : MonoBehaviour
+    {
+        private readonly int OpenTrigger = Animator.StringToHash("Open");
+
+        [SerializeField] private Animator _animator;
+        
+        public void Open()
+        {
+            _animator.SetTrigger(OpenTrigger);
+        }        
+    }
+}
